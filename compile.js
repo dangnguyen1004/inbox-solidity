2d692ced1266af8ad566821b706dd213ca21d9d3
DangNguyen <nguyendangit04@gmail.com>
2023-04-04 23:39:04 +0700

    deploy Inbox with solc

--- compile.js
+++ compile.js
@@ -1,8 +1,28 @@
-const path  = require('path')
-const fs = require('fs')
-const solc = require('solc')
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
-const source = fs.readFileSync(inboxPath, 'utf-8')
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf-8");
 
-console.log(solc.compile(source, 1))
+var input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+const InboxContract = output.contracts["Inbox.sol"].Inbox;
+
+module.exports = InboxContract;
